40940c25b08ba0a22bee8495506fc967fe23ecc8
Yuvadi29 <adityatrivedi612@gmail.com>
2024-07-04 15:43:23 +0530

    Weather Observation Station Q11

---
+++ Weather_Observation_Station_11.sql
@@ -0,0 +1,20 @@
+/*
+Query the list of CITY names from STATION that do not start and end with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+
+-- +-------------+--------------+
+-- | Field       | Type         |
+-- +-------------+--------------+
+-- | ID           | NUMBER       |
+-- | CITY        | VARCHAR2(21) |
+-- | STATE      | VARCHAR2(2)   |
+-- | LAT_N     | NUMBER         |
+-- | LONG_W   | NUMBER          |
+ */
+SELECT DISTINCT
+    CITY
+FROM
+    STATION
+WHERE
+    CITY NOT RLIKE '^[aeiou]'
+    OR CITY NOT REGEXP '[aeiou]$'
